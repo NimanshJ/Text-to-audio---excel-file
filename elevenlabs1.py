13446ae7431b67ac5fc92bd0ad92d9b717e9b636
Nimansh jain <jainnimansh@gmail.com>
2025-12-21 17:21:02 +0530

    ready

---
+++ elevenlabs1.py
@@ -0,0 +1,35 @@
+from elevenlabs.client import ElevenLabs
+from openpyxl import load_workbook
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+name = input("File name: ")
+leng = int(input("Length: "))
+
+client = ElevenLabs(api_key=os.getenv("KEY"))
+workbook = load_workbook(f"{name}.xlsx")
+sheet = workbook.active
+for i in range(0,leng):
+
+    audio = b"".join(
+        client.text_to_speech.convert(
+            text=sheet[f"{os.getenv("COLUMN")}{i+int(os.getenv("STARTING_ROW"))}"].value,
+            voice_id=os.getenv("VOICE_ID"),
+            model_id="eleven_multilingual_v2",
+            voice_settings={
+                "stability": float(os.getenv("STABILITY")),
+                "similarity_boost": float(os.getenv("SIMILARITY")),
+                "style": float(os.getenv("STYLE")),
+                "use_speaker_boost": True
+            }
+        )
+    )
+
+    with open(f"{i}.mp3", "wb") as f:
+        f.write(audio)
+
+print("Saved test.mp3")
+
+
